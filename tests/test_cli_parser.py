086681280d4337b7148c271b827910548b9b877f
spikkie <spikkie@gmail.com>
2026-05-26 14:37:41 +0200

    chatgpt_claudecode_workflow_v0.0.276.13.zip

--- tests/test_cli_parser.py
+++ tests/test_cli_parser.py
@@ -171,7 +171,7 @@ def test_parser_version_option_outputs_release(capsys) -> None:
     except SystemExit as exc:
         assert exc.code == 0
     out = capsys.readouterr().out
-    assert "0.0.276.12" in out
+    assert "0.0.276.13" in out
     assert "promptbranch" in out
 
 
